c07472a5bafa973f6d1a7ba03f6cbde73fa22237
root <root@ASPIRE>
2023-07-05 02:30:33 +0300

    FF

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -62,9 +62,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'django'),
-        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'Dima'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', ''),
+        'HOST': 'db'
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
